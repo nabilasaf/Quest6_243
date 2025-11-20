8f8bc682d10f5dab59246e259b88e9dc96ebbe5d
nabilasaf <nabilasafitri861@gmail.com>
2025-11-20 12:24:16 +0700

    menambahkan val item di TampilSiswa

--- app/src/main/java/com/example/mvvm/view/TampilanSiswa.kt
+++ app/src/main/java/com/example/mvvm/view/TampilanSiswa.kt
@@ -2,13 +2,19 @@ package com.example.mvvm.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.example.mvvm.model.Siswa
-
+import com.example.mvvm.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBack: () -> Unit
 ) {
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(id = R.string.alamat), statusUiSiswa.alamat),
+    )
 
 }
