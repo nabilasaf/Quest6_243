11a06d163b6eacc79af412d380cce582fa99dbf8
nabilasaf <nabilasafitri861@gmail.com>
2025-11-20 14:02:22 +0700

    membuat scaffold untuk navigasi

--- app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
@@ -1,10 +1,14 @@
 package com.example.mvvm.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.mvvm.viewmodel.SiswaViewModel
 
@@ -19,4 +23,13 @@ fun SiswaApp(
     modifier: Modifier,
     viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
-) {}
+) {
+    Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ){}
+    }
+}
