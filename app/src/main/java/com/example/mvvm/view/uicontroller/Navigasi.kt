bfe6461c102ba6872fd04da958a7a47577207dfa
nabilasaf <nabilasafitri861@gmail.com>
2025-11-20 13:59:09 +0700

    membuat fun siswaApp navigasi

--- app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/mvvm/view/uicontroller/Navigasi.kt
@@ -1,6 +1,22 @@
 package com.example.mvvm.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.mvvm.viewmodel.SiswaViewModel
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {}
