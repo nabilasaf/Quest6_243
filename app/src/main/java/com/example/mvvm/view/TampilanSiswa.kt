85e50dcf0cb5f1ec5ee039c1a52b8c2f5ce67306
nabilasaf <nabilasafitri861@gmail.com>
2025-11-20 12:14:56 +0700

    membuat TampilSiswa

---
+++ app/src/main/java/com/example/mvvm/view/TampilanSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.mvvm.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.mvvm.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBack: () -> Unit
+) {
+
+}
